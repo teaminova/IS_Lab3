8312fc5b94dc7d3bafaf32f99ba1e3e18e1afef2
Tea Minova <tea.minova11@gmail.com>
2024-04-26 00:50:51 +0200

    Added Domain package

---
+++ IntegratedSystems.Domain/Dto/VaccineDto.cs
@@ -0,0 +1,18 @@
+﻿using IntegratedSystems.Domain.Domain_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegratedSystems.Domain.Dto
+{
+    public class VaccineDto
+    {
+        public string? Manufacturer { get; set; }
+        public Guid? PatientId { get; set; }
+        public Guid? VaccinationCenter { get; set; }
+        public List<string>? AllManufacturers { get; set; }
+        public List<Patient>? AllPatients { get; set; }
+    }
+}
